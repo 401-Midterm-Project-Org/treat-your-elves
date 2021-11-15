1d216df82f0259b526810f033f76d4f6a1423ada
Sarah Creager <sarah.f.jamieson@gmail.com>
2021-11-15 15:33:14 -0800

    added TODO for table relationship

--- src/models/index.js
+++ src/models/index.js
@@ -9,6 +9,10 @@ const DATABASE_URL = 'sqlite:memory:';
 
 const sequelize = new Sequelize(DATABASE_URL)
 
+// TODO: This will go into models/ index.js These models will not work with another database technology.
+// list.hasMany(listItem, { foreignKey: 'listId', sourceKey: 'id' });
+// listItem.belongsTo(list, { foreignKey: 'listId', targetKey: 'id' });
+
 module.exports = {
   db:sequelize,
   users: userModel(sequelize, DataTypes),
