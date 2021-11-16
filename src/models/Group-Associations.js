bf8950ffef785e33e41a15788c9c56dd63697022
Sarah Creager <sarah.f.jamieson@gmail.com>
2021-11-16 10:55:00 -0800

    adds group/user associations model

---
+++ src/models/Group-Associations.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const groupAssociationsModel = (sequelize, DataTypes) => {
+  const model = sequelize.define('GroupAssociations', {
+
+    groupId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+
+    userRole: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    }
+    
+  })
+
+  return model;
+}
+
+module.exports = groupAssociationsModel;
