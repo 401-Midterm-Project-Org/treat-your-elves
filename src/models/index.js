6b6e7d1ed026767d1febe6bc16948fceb8d6db6f
Sarah Creager <sarah.f.jamieson@gmail.com>
2021-11-15 16:37:54 -0800

    connecting databases together

--- src/models/index.js
+++ src/models/index.js
@@ -4,17 +4,35 @@ const { Sequelize, DataTypes } = require('sequelize')
 
 const userModel = require('./Users.js');
 const groupModel = require('./Groups.js');
+const wishListModel = require('./Wishlist.js');
+const listItemModel = require('./List-Item.js');
 
 const DATABASE_URL = 'sqlite:memory:';
 
 const sequelize = new Sequelize(DATABASE_URL)
 
-// TODO: This will go into models/ index.js These models will not work with another database technology.
-// list.hasMany(listItem, { foreignKey: 'listId', sourceKey: 'id' });
-// listItem.belongsTo(list, { foreignKey: 'listId', targetKey: 'id' });
+// instantiate our DB with our models
+const groups = groupModel(sequelize, DataTypes);
+const users = userModel(sequelize, DataTypes);
+const wishList = wishListModel(sequelize, DataTypes);
+const listItem = listItemModel(sequelize, DataTypes);
+
+// TODO: connect databases 
+
+// connecting user to group
+groups.hasMany(users, { foreignKey: 'userId', sourceKey: 'id' });
+users.belongsTo(groups, { foreignKey: 'userId', targetKey: 'id' });
+
+// connecting wishlist to user
+users.hasMany(wishList, { foreignKey: 'wishListId', sourceKey: 'id' });
+wishList.belongsTo(users, { foreignKey: 'wishListId', targetKey: 'id' });
+
+// connecting list item to wishlist
+wishList.hasMany(listItem, { foreignKey: 'listId', sourceKey: 'id' });
+listItem.belongsTo(wishList, { foreignKey: 'listId', targetKey: 'id' });
 
 module.exports = {
   db:sequelize,
-  users: userModel(sequelize, DataTypes),
-  groups: groupModel(sequelize, DataTypes)
+  users,
+  groups
 };
