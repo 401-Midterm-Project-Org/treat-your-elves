367672484eca2634d9dbadcead7f90cb963bd26b
Sarah Creager <sarah.f.jamieson@gmail.com>
2021-11-18 17:56:01 -0800

    basicAuth test passing

---
+++ __tests__/basicAuth.test.js
@@ -0,0 +1,58 @@
+'use strict';
+
+const middleware = require('../src/middleware/basic.js');
+const { db, users } = require('../src/models/index.js');
+const supertest = require("supertest");
+const { server } = require("../src/server.js");
+const mockRequest = supertest(server);
+
+let user = {
+  admin: { username: 'test', password: 'test', name: 'test' },
+};
+
+// Pre-load our database with fake users
+beforeAll(async () => {
+  await db.sync();
+  await users.create(user.admin);
+});
+
+afterAll(async () => {
+  await db.drop();
+})
+
+describe('Basic Auth Middleware', () => {
+
+  const req = {};
+  const res = {
+    status: jest.fn(() => res),
+    send: jest.fn(() => res)
+  }
+  const next = jest.fn();
+
+  it('Fails to login user with the incorrect credentials', () => {
+
+    req.headers = {
+      authorization: 'Basic YWRtaW46Zm9v',
+    };
+
+    return middleware(req, res, next)
+      .then(() => {
+        expect(next).not.toHaveBeenCalled();
+        expect(res.status).toHaveBeenCalledWith(403);
+      });
+
+  });
+
+  it("Successfully logs in user with basicAuth", async () => {
+    const response = await mockRequest
+      .post("/signin")
+      .auth(user.admin.username, user.admin.password);
+
+    const userObject = response.body;
+    expect(response.status).toBe(200);
+    expect(userObject.token).toBeDefined();
+    expect(userObject.user.id).toBeDefined();
+    expect(userObject.user.username).toEqual('test');
+  });
+
+});
