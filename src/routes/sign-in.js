f09d3d3d3fedb7ada4fae2b4d3ff827b7bdefcbb
Justin Hamerly <84735695+JustinHamerly@users.noreply.github.com>
2021-12-27 18:46:19 -0800

    Revert "restructuring backend for auth0"

--- src/routes/sign-in.js
+++ src/routes/sign-in.js
@@ -5,6 +5,7 @@ const express = require('express');
 const authRouter = express.Router();
 
 const { users } = require('../models/index.js');
+// const basicAuth = require('../middleware/basic.js');
 
 authRouter.post('/signin', async (request, response, next) => {
   const email = request.body.email.toLowerCase();
@@ -12,11 +13,14 @@ authRouter.post('/signin', async (request, response, next) => {
   try {
     let user = await users.findOne({ where: { email } });
 
+    // instead of throwing error, create a new user
     if (!user) {
+      // create new user
       user = await users.create(request.body);
     }
 
     console.log(user);
+    // console.log(request.body);
 
     response.status(200);
     response.send(user);
