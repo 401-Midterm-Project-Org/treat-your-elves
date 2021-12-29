8ae5a2df5b306f476c9fd4d6c7eb61432ea34332
KATYROFFE <KATYROFFE@GMAIL.COM>
2021-12-28 18:36:18 -0800

    Updates to gmail account instead of test account.

--- src/routes/mailingRoute.js
+++ src/routes/mailingRoute.js
@@ -11,8 +11,8 @@ emailRouter.use(bodyParser.urlencoded({extended: true}))
 emailRouter.use(bodyParser.json())
 
 emailRouter.post("/sendmail", cors(), async (req, res) => {
-  let { text } = req.body;
   console.log(req.body)
+  let { to, subject, html } = req.body;
   const transport = nodemailer.createTransport({
     host: process.env.MAIL_HOST,
     port: process.env.MAIL_PORT,
@@ -24,19 +24,9 @@ emailRouter.post("/sendmail", cors(), async (req, res) => {
 
   await transport.sendMail({
     from: process.env.MAIL_FROM,
-    to: "test@test.com",
-    subject: "test email",
-    html: `<div className="email" style="
-    border: 1px solid black;
-    padding: 20px;
-    font-family: sans-serif;
-    line-height: 2;
-    font-size: 20px;">
-    <h2>An Email Is Here!</h2>
-    <p>${text}</p>
-    <p>All the best, Elf Notification Squad</p>
-    </div>`
-
+    to,
+    subject,
+    html
   })
 })
 
